2157538605e370d7c8a09d84279131ae9c3c617b
stevemac321 <stevemac321@live.com>
2024-07-12 16:51:17 +0000

    added a new testcase to zero2end

--- tests/test_zeroes2end.rs
+++ tests/test_zeroes2end.rs
@@ -2,12 +2,23 @@ extern crate test_geeks;
 use test_geeks::zeroes2end::zeroes2end;
 
 #[test]
-fn test_zeroes2end()
+fn test_zeroes2end() 
 {
-    let mut arr = [1, 0, 2, 0, 3, 0, 4, 5];
-    let newlen = zeroes2end(&mut arr);
-    assert_eq!(newlen, 5);
-    for i in newlen..arr.len() {
-        assert_eq!(arr[i], 0);
+    {
+        let mut arr = [1, 0, 2, 0, 3, 0, 4, 5];
+        let newlen = zeroes2end(&mut arr);
+        assert_eq!(newlen, 5);
+        for i in newlen..arr.len() {
+            assert_eq!(arr[i], 0);
+        }
+    }
+
+    {
+        let mut arr = [1, 0, 2, 0, 3, 0, 4, 5, 0, 0, 0,9, 9, 0];
+        let newlen = zeroes2end(&mut arr);
+        assert_eq!(newlen, 7);
+        for i in newlen..arr.len() {
+            assert_eq!(arr[i], 0);
+        }
     }
 }
