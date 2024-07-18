13a0604494fff438f43c5ecd6921622d4c9955b5
stevemac321 <stevemac321@live.com>
2024-07-17 18:30:42 -0700

    added a test

--- tests/test_non_repeating.rs
+++ tests/test_non_repeating.rs
@@ -7,3 +7,9 @@ fn test_non_repeating() {
     let result = non_repeating(nums);
     assert_eq!(&result, &[3, 4]);
 }
+#[test]
+fn test_non_repeating2() {
+    let nums = vec![1, 2, 3, 2, 1, 4, 6, 7, 8, 4];
+    let result = non_repeating(nums);
+    assert_eq!(&result, &[3, 6, 7, 8]);
+}
